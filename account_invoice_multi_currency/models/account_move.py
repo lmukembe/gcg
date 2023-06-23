5151e25ad5e48a2fe8ca2d73ce0249d817fe2a5a
Vamosdrc-IT <132292264+lmukembe@users.noreply.github.com>
2023-06-23 17:39:33 +0200

    Delete account_invoice_multi_currency directory

--- account_invoice_multi_currency/models/account_move.py
+++
@@ -1,9 +0,0 @@
-from odoo import fields, models
-
-
-class AccountMove(models.Model):
-    _inherit = 'account.move'
-
-    invoice_multi_currency_ids = fields.Many2many('res.currency',
-                                                  string='Invoice Currencies',
-                                                  related='company_id.invoice_multi_currency_ids')
--- account_invoice_multi_currency/models/res_company.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models
-
-
-class ResCompany(models.Model):
-    _inherit = 'res.company'
-
-    invoice_multi_currency_ids = fields.Many2many('res.currency',
-                                                  string='Invoice Currencies')
